07da653bcd9d495751114d9cc6031ff1e8ecaca4
Harry Grillo <harry@redbassett.com>
2016-03-24 02:59:45 -0400

    Add missing return to settings intent

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
