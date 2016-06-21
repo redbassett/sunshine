b929d201e2432838ab7b4fe34a10e5c4007b59ec
Harry Grillo <harry@redbassett.com>
2016-06-21 02:40:14 -0400

    Update colors from sunshine colors to material design palette

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -569,6 +569,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                     // notifications.  Just throw in some data.
                     NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(getContext())
+                                    .setColor(resources.getColor(R.color.primary_light))
                                     .setSmallIcon(iconId)
                                     .setContentTitle(title)
                                     .setContentText(contentText);
