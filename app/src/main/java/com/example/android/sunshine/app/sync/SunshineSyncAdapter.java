4c01b1ca7f9707c208faa9600a346de3be4467f9
Harry Grillo <harry@redbassett.com>
2016-06-22 15:59:22 -0400

    Dynamically set widget data

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -55,6 +55,8 @@ import java.util.concurrent.ExecutionException;
 public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     public final String LOG_TAG = SunshineSyncAdapter.class.getSimpleName();
     public static final String LOCATION_QUERY_EXTRA = "lqe";
+    public static final String ACTION_DATA_UPDATED =
+            "com.example.android.sunshine.app.ACTION_DATA_UPDATED";
 
     // Interval at which to sync with the weather, in milliseconds.
     // 60 seconds (1 minute) * 180 = 3 hours
@@ -408,6 +410,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                         WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
                         new String[] {Long.toString(dayTime.setJulianDay(julianStartDay - 1))});
 
+                updateWidgets();
                 notifyWeather();
             }
 
@@ -510,6 +513,14 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
     public static void initializeSyncAdapter(Context context) {
         getSyncAccount(context);
+    }
+
+    private void updateWidgets() {
+        Context context = getContext();
+        // Setting the package ensures that only components in our app will receive the broadcast
+        Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED)
+                .setPackage(context.getPackageName());
+        context.sendBroadcast(dataUpdatedIntent);
     }
 
     private void notifyWeather() {
